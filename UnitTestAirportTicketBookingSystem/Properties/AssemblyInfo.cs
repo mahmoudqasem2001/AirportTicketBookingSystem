44e9ea3d1f464e428e98386393429d2d00b520c4
mahmoudqasem2001 <mahmoodqasim543@gmail.com>
2024-10-15 23:32:43 +0300

    Setup testing project for Airport Ticket Booking System

---
+++ UnitTestAirportTicketBookingSystem/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestAirportTicketBookingSystem")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestAirportTicketBookingSystem")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("98a4caaf-52ae-429e-bf8d-2ac050060a41")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
